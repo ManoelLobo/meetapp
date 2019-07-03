3c07a9b6e11c59822f41839a9e388380dd0128ab
Manoel <manoel.lobo@gmail.com>
2019-07-03 15:49:31 -0300

    auth middleware

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ status: 'ok' }));
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/test', (req, res) => res.json({ status: 'ok' }));
 
 export default routes;
